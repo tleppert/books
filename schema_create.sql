5591bca1e4de4dd7472f2ff847e767702983beb7
Tom Leppert <tleppert@gmail.com>
2016-08-24 00:23:29 -0400

    Add files via upload
    Change Varchar to NVarChar to allow for extended character set in data.  Rename table Type_Defn to Format_Defn to avoid conflict with MSSQL table.  Add optional drop of all tables to script. Add optional truncate of all tables to script. Add insert of default values into Dimension tables.

--- schema_create.sql
+++ schema_create.sql
@@ -1,10 +1,96 @@
 USE books;  
 GO
 
+/*
+Drop all objects for database recreate 
+*/
+
+/*
+DROP TABLE dbo.Series_Defn;
+GO
+
+DROP TABLE dbo.Author;
+GO
+
+DROP TABLE dbo.author_stg;
+GO
+
+DROP TABLE dbo.Book;
+GO
+
+DROP TABLE dbo.Book_Author;
+GO
+
+DROP TABLE dbo.Book_Comment;
+GO
+
+DROP TABLE dbo.Book_Loan;
+GO
+
+DROP TABLE dbo.Book_Series;
+GO
+
+DROP TABLE dbo.book_stg;
+GO
+
+DROP TABLE dbo.Category_Defn;
+GO
+
+DROP TABLE dbo.nBook_stg;
+GO
+
+DROP TABLE dbo.Publisher_Defn;
+GO
+
+DROP TABLE dbo.Rating_Defn;
+GO
+
+DROP TABLE dbo.Status_Defn;
+GO
+
+DROP TABLE dbo.Sub_Category_Defn;
+GO
+
+*/
+
+/* Truncate all tables for reload */
+
+/*
+TRUNCATE TABLE Author; 
+GO
+TRUNCATE TABLE Author_Stg; 
+GO
+TRUNCATE TABLE Book; 
+GO
+TRUNCATE TABLE Book_Author; 
+GO
+TRUNCATE TABLE Book_Comment; 
+GO
+TRUNCATE TABLE Book_Loan; 
+GO
+TRUNCATE TABLE Book_Series; 
+GO
+TRUNCATE TABLE Category_Defn; 
+GO
+TRUNCATE TABLE Format_Defn; 
+GO
+TRUNCATE TABLE Publisher_Defn; 
+GO
+TRUNCATE TABLE Rating_Defn; 
+GO
+TRUNCATE TABLE Series_Defn; 
+GO
+TRUNCATE TABLE Status_Defn; 
+GO
+TRUNCATE TABLE Sub_Category_Defn; 
+GO
+
+*/
+
 -- Create dimension tables
 CREATE TABLE Series_Defn      
 (series_id   Integer      Not Null,
- series_name VarChar(256) Not Null,
+ series_name NVarChar(256) Not Null,
  CONSTRAINT Series_Defn_PK PRIMARY KEY CLUSTERED (series_id)
 ); 
 GO
@@ -16,10 +102,10 @@ CREATE TABLE Publisher_Defn
 );
 GO  
 
-CREATE TABLE Type_Defn     
-(type_id   Integer     Not Null,   
- type_name VarChar(50) Not Null,
- CONSTRAINT Type_Defn_PK PRIMARY KEY CLUSTERED (type_id)
+CREATE TABLE Format_Defn     
+(format_id   Integer     Not Null,   
+ format_name VarChar(50) Not Null,
+ CONSTRAINT Type_Defn_PK PRIMARY KEY CLUSTERED (format_id)
 );
 GO  
  
@@ -56,10 +142,11 @@ GO
 -- Create fact tables
 CREATE TABLE Book 
 (book_id         Integer      Not Null,
- title           VarChar(512) Not Null,
+ title           NVarChar(512) Not Null,
  publish_month   Char(2)      Not Null,
  publish_year    Char(4)      Not Null,
  isbn_asin       VarChar(20)  Not Null,
+ format_id       Integer      Not Null,
  category_id     Integer      Not Null,
  sub_category_id Integer      Not Null,
  publisher_id    Integer      Not Null,
@@ -71,9 +158,9 @@ GO
  
 CREATE TABLE Author 
 (author_id  Integer      Not Null,
- first_name VarChar(256) Not Null,
- last_name  VarChar(256) Not Null,
- full_name  VarChar(512) Not Null,
+ first_name NVarChar(256) Not Null,
+ last_name  NVarChar(256) Not Null,
+ full_name  NVarChar(512) Not Null,
  CONSTRAINT Author_PK PRIMARY KEY CLUSTERED (author_id)
 );
 GO    
@@ -88,7 +175,7 @@ GO
 CREATE TABLE Book_Series 
 (book_id       Integer    Not Null,
  series_id     Integer    Not Null,
- series_number VarChar(4) Not Null,
+ series_number VarChar(10) Not Null,
  CONSTRAINT Book_Series_PK PRIMARY KEY CLUSTERED (book_id, series_id)
 );
 GO  
@@ -108,3 +195,40 @@ CREATE TABLE Book_Loan
  CONSTRAINT Book_Loan_PK PRIMARY KEY CLUSTERED (book_id,loan_date)
 );
 GO  
+
+/* Staging table for intitial data load */
+CREATE TABLE Author_Stg
+(author_id  Integer      Not Null,
+ full_name  NVarChar(512) Not Null
+);
+GO  
+
+/* Load defaults for dimensions */
+INSERT INTO Category_Defn (category_id, category_name)
+VALUES (0, 'None');
+GO
+
+INSERT INTO Sub_Category_Defn (sub_category_id, sub_category_name)
+VALUES (0, 'None');
+GO
+
+INSERT INTO Format_Defn (format_id, format_name)
+VALUES (0, 'Unknown');
+GO
+
+INSERT INTO Publisher_Defn (publisher_id, publisher_name)
+VALUES (0, 'Unknown');
+GO
+
+INSERT INTO Rating_Defn (rating_id, rating_desc)
+VALUES (0, 'None');
+GO
+
+INSERT INTO Status_Defn (status_id, status_descr)
+VALUES (0, 'None');
+GO
+
+/* Add special Author of Unknown */
+INSERT INTO Author (author_id, first_name, last_name, full_name)
+VALUES (0, 'Unknown', ' ', 'Unknown');
+GO
